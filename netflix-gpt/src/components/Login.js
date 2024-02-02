1e50f1f6de5c8cf87a26e44f882badf47475d8e9
Raja Kumar <raja.kumar@chrobinson.com>
2024-02-02 20:51:56 +0530

    Fixed bug in Form

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -18,9 +18,9 @@ const Login = () => {
       </div>
       <form className="w-3/12 absolute p-12 bg-black m-36 mx-auto right-0 left-0 text-white bg-opacity-80">
         <h1 className="font-bold text-3xl py-4">
-          {!isSignInForm ? "Sign In" : "Sign Up"}
+          {!isSignInForm ? "Sign Up" : "Sign In"}
         </h1>
-        {isSignInForm && (
+        {!isSignInForm && (
           <input
             type="text"
             placeholder="Full Name"
@@ -39,12 +39,12 @@ const Login = () => {
           className="p-2 my-4 w-full bg-gray-500"
         />
         <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
-          {!isSignInForm ? "Sign In" : "Sign Up"}
+          {!isSignInForm ? "Sign Up" : "Sign In"}
         </button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
-          {!isSignInForm
+          {isSignInForm
             ? " New to Netflix? Sign Up Now"
-            : "Already a User Sign in Now"}
+            : "Already a User?  Sign in Now"}
         </p>
       </form>
     </div>
